4f69385a0caa91ff32bddf05be3c01f4394b5fb2
1103syike <1103syike@gmail.com>
2026-01-19 11:08:22 +0800

    chore(angular): 專案配置完成。

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Header } from './layout/header/header';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
+  imports: [RouterOutlet,Header],
+  template: '<app-header/>',
   styleUrl: './app.css'
 })
 export class App {
